c352a7d8cbce16fad80cf954bb04e6c9ceddf8c5
Susan Meyers <smmeyers24@gmail.com>
2018-10-15 17:47:49 +0000

    automatically backed up by learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -3,4 +3,11 @@ module MetaDancing
   def metadata
     "This class produces objects that love to dance."
   end
+  
+  module MetaDancing
+ 
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
 end
